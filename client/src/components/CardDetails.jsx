24362b4eb2c5ea8f2fff53aed0f4881c728f64d0
AjayGupta1822 <142434768+AjayGupta1822@users.noreply.github.com>
2024-07-11 01:57:16 +0530

    Update CardDetails.jsx

--- client/src/components/CardDetails.jsx
+++ client/src/components/CardDetails.jsx
@@ -28,7 +28,7 @@ function CardDetails() {
     if (result) {
       try {
         toast.promise(
-          axios.delete(`https://friends-failsapp.vercel.app/listings/${data._id}`),
+          axios.delete(`https://backend-cat-fails.vercel.app/listings/${data._id}`),
           {
             loading: "Loading...",
             success: () => {
